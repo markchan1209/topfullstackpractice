af2858ba580a076feb4397ce649282d1022f79c1
MarkChan <markgame86@gmail.com>
2020-01-12 12:13:24 +0800

    Add episode include typegoose

--- server/libs/db/src/Models/episode.model.ts
+++ server/libs/db/src/Models/episode.model.ts
@@ -1,5 +1,10 @@
-import { prop } from "@typegoose/typegoose";
+import { prop, modelOptions } from "@typegoose/typegoose";
 
+@modelOptions({
+    schemaOptions:{
+        timestamps: true
+    }
+})
 export class Episode {
 
     @prop()
